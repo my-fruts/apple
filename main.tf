ee7fe6bcd791f8dcf7e2888284c30cea695717a9
galindonkov <43785350+galindonkov@users.noreply.github.com>
2018-10-15 11:40:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "null_resource" "helloWorld" {
+resource "null_resource" "helloWorld234" {
+  provisioner "local-exec" {
+    command = "echo hello world"
+  }
+}
+
+resource "null_resource" "helloWorld112" {
   provisioner "local-exec" {
     command = "echo hello world"
   }
